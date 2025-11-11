e80a0839841e3ce4cdd5e5cfc889d74db1a99565
Gerardo Jesus <grrdrito@gmail.com>
2025-11-11 09:19:42 -0600

    se agrega funcion de editar y marcacion de tarea terminada

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [task, setTask] = useState('')
     const [tasks, setTasks] = useState(()=>{
         const stored = localStorage.getItem('tasks') //funcion para que al inicio de cada render el estado se revise si hay items  los parsea sino devuelve un array vacio
-        if(stored){ // sino incio mi estado de esta forma el esatado no se guarda en localstoraged
+        if(stored){ // sino incio mi estado de esta forma el esatado no se guarda en localstorage
             return JSON.parse(stored)
         }else{
             return []
@@ -14,8 +14,8 @@ function App() {
     })
 
     useEffect(() => {
-        localStorage.setItem('tasks', JSON.stringify(tasks))// cada que se actualiza mi estados tasks
-    },[tasks])
+        localStorage.setItem('tasks', JSON.stringify(tasks))// cada que se actualiza mi estado tasks
+    },[tasks])//porque si tasks se actualiza quiere decir que hay una tarea que entra a local storage
 
     const writeTask=(event)=> setTask(event.target.value)
 
@@ -24,7 +24,11 @@ const addTaskEnter=(event)=> {
          addTask()
       }
 }
-
+const editTask=(id)=> {
+      const newText=window.prompt('Enter new task title');
+      if(!newText) return
+      setTasks(tasks.map(task=> task.id === id ? {...task, text:newText} : task))
+}
     const addTask= ()=>{
         if (task=== '') return
         setTasks([...tasks, //se copian todas las tareas y se agregan la nueva tarea en forma de objeto sino se copian el setTasks actualiza y las deja libres, las eliminaria el garbage collector
@@ -38,6 +42,9 @@ const addTaskEnter=(event)=> {
     const deleteTask= (id)=>{
      setTasks(tasks.filter(task => task.id !== id))// hazme un nuevo array con todos los elementos que sean diferentes al id seleccionado
     }
+    const changeStateTask=(id)=> {
+      setTasks(tasks.map(task => id === task.id ? { ...task, completed:!task.completed } : task))
+    }
 
   return (
         <div>
@@ -46,8 +53,12 @@ const addTaskEnter=(event)=> {
             <button onClick={addTask} >Add Task</button>
             <ul>
                 {tasks.map((t) => (
-                    <li key={t.id}>{t.text}
-                        <button onClick={()=> deleteTask(t.id)}>❌</button></li>
+                    <li key={t.id} style={{textDecoration: t.completed ? 'line-through' : 'none'}}>
+                        <span onClick={()=>changeStateTask(t.id)} style={{cursor:'pointer'}}>{t.text}
+                        </span>
+                        <button onClick={()=> deleteTask(t.id)}>❌</button>
+                    <button onClick={()=>editTask(t.id)}>✍️</button>
+                    </li>
                     ))}
 
             </ul>
